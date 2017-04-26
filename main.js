7b6adfc0039cde7154089540109837d3958142db
z748807622 <748807622@qq.com>
2017-04-26 18:42:19 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var find = require('./models/find');
+
+var app = express();
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use(express.static(__dirname+'/public'));
+
+app.post('/find',function (req,res) {
+    var name = req.body.name;
+    var method = req.body.method;
+    //res.send('123');
+    console.log(name+method)
+    switch(method){
+    	case 'baidu':find.baidu(name,function(data){
+    		res.json(data);
+    		return;
+    	})
+    	break;
+    	case 'kugou':find.kugou(name,function(data){
+    		res.json(data);
+    		return;
+    	})
+    	break;
+    	default:
+    }
+});
+app.listen(3000);
